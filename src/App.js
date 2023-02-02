b8cafb614062072fbf40a5d82f73d4ddde4b8310
AUTUMN <krshlee22@naver.com>
2023-02-03 00:17:10 +0900

    페이지 새로고침 시 useRef(0)때문에 nextId가 0으로 초기화 되는 오류 수정

--- src/App.js
+++ src/App.js
@@ -19,14 +19,20 @@ function App() {
   }, []);
 
   // todolist 상태관리
+  let savedId = 0;
   const [todoLists, setTodoLists] = useState(() => {
     if(typeof window !== "undefined"){
       const savedtodo = window.localStorage.getItem("todo")
-      return (savedtodo !== null) ? JSON.parse(savedtodo) : [""]
+      if(JSON.parse(savedtodo)[0] !== undefined) {
+        savedId = JSON.parse(savedtodo)[JSON.parse(savedtodo).length-1].id+1
+        return JSON.parse(savedtodo)
+      }else {
+        return []
+      }
     }
   });
   
-  const nextId = useRef(0);
+  const nextId = useRef(savedId);
   const onCreate = useCallback ( e => {
     e.preventDefault();
     const todoList = {
@@ -45,7 +51,6 @@ function App() {
   const onRemove = useCallback ( id => {
     setTodoLists(todoLists => todoLists.filter(todoList => todoList.id !== id));
   }, []);
-  /// 일부 todo를 삭제 후 재입력하였을때 새로운 todoLIst의 id값과 기존에 있던 list의 id값이 동일하여 같은 아이디값을 삭제하게 되는 오류 존재
 
 
   // 리스트 저장함수
